5f25963a81e51894e066c6ecdf12adbf77128147
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-09-04 01:07:22 -0400

    Stylistic cleanup and proper access control

--- lib/xbitsy/tokenizer.ex
+++ lib/xbitsy/tokenizer.ex
@@ -2,27 +2,27 @@ defmodule Xbitsy.Tokenizer do
 
   def lex(source), do: do_lex(source, [])
 
-  def do_lex(source = << first :: utf8, tail :: binary >>, acc) do
-    {lexeme, remaining} = take_matching(source, matcher_for(first), "")
+  defp do_lex(source = << first :: utf8, tail :: binary >>, acc) do
+    {lexeme, remaining} = source |> take_matching(matcher_for(first), "")
     do_lex(remaining, [lexeme | acc])
   end
 
-  def do_lex(<<>>, acc) do
+  defp do_lex(<<>>, acc) do
     acc |> Enum.reverse
   end
 
-  def take_matching(source = << first :: utf8, tail :: binary >>, matches?, acc) do
+  defp take_matching(source = << first :: utf8, tail :: binary >>, matches?, acc) do
     cond do
-      matches?.(first) -> take_matching(tail, matches?, << acc::binary, first::utf8 >>)
+      matches?.(first) -> tail |> take_matching(matches?, << acc::binary, first::utf8 >>)
       true -> {acc, source}
     end
   end
 
-  def take_matching(<<>>, _matches?, acc) do
+  defp take_matching(<<>>, _matches?, acc) do
     {acc, <<>>}
   end
 
-  def matcher_for(char) do
+  defp matcher_for(char) do
     cond do
       is_white?(char) -> &is_white?/1
       is_ident?(char) -> &is_ident?/1
@@ -31,8 +31,6 @@ defmodule Xbitsy.Tokenizer do
   end
 
   # MATCHERS
-  def is_white?(char) when char == ?\s or char == ?\t or char == ?\n, do: true
-  def is_white?(_char), do: false
-  def is_ident?(char) when (char >= ?A and char <= ?Z) or (char >= ?a and char <= ?z), do: true
-  def is_ident?(_char), do: false
+  defp is_white?(char), do: char == ?\s or char == ?\t or char == ?\n
+  defp is_ident?(char), do: (char >= ?A and char <= ?Z) or (char >= ?a and char <= ?z)
 end
