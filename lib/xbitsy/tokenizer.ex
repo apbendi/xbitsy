c2c0fda41b01fc1c176ba39c3c7358a8c61d7b38
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-09-04 00:48:31 -0400

    Define a generic function for taking the next lexeme

--- lib/xbitsy/tokenizer.ex
+++ lib/xbitsy/tokenizer.ex
@@ -4,7 +4,7 @@ defmodule Xbitsy.Tokenizer do
 
   def do_lex(source = << first :: utf8, tail :: binary >>, acc) do
     if first == ?\s do
-      {whitespace, remaining} = lex_white(source, "")
+      {whitespace, remaining} = take_matching(source, &is_white?/1, "")
       do_lex(remaining, [whitespace | acc])
     else
       {identifier, remaining} = lex_ident(source, "")
@@ -16,10 +16,21 @@ defmodule Xbitsy.Tokenizer do
     acc |> Enum.reverse
   end
 
+  def take_matching(source = << first :: utf8, tail :: binary >>, matches?, acc) do
+    cond do
+      matches?.(first) -> take_matching(tail, matches?, << acc::binary, first::utf8 >>)
+      true -> {acc, source}
+    end
+  end
+
+  def take_matching(<<>>, _matches?, acc) do
+    {acc, <<>>}
+  end
+
   def lex_white(source = << first :: utf8, tail :: binary >>, acc) do
-    case first do
-      ?\s -> lex_white(tail, << acc::binary, first::utf8 >>)
-      _ -> {acc, source}
+    cond do
+      is_white?(first) -> lex_white(tail, << acc::binary, first::utf8 >>)
+      true -> {acc, source}
     end
   end
 
@@ -29,7 +40,7 @@ defmodule Xbitsy.Tokenizer do
 
   def lex_ident(source = << first :: utf8, tail :: binary >>, acc) do
     cond do
-      is_ident(first) -> lex_ident(tail, << acc::binary, first::utf8 >>)
+      is_ident?(first) -> lex_ident(tail, << acc::binary, first::utf8 >>)
       true -> {acc, source}
     end
   end
@@ -38,6 +49,9 @@ defmodule Xbitsy.Tokenizer do
     {acc, <<>>}
   end
 
-  def is_ident(char) when (char >= ?A and char <= ?Z) or (char >= ?a and char <= ?z), do: true
-  def is_ident(_char), do: false
+  # MATCHERS
+  def is_white?(char) when char == ?\s or char == ?\t or char == ?\n, do: true
+  def is_white?(_char), do: false
+  def is_ident?(char) when (char >= ?A and char <= ?Z) or (char >= ?a and char <= ?z), do: true
+  def is_ident?(_char), do: false
 end
