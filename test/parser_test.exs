fe4f234e00784e11a606f38979e016adecd8d4d4
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-09-07 11:59:43 -0400

    Add some helper methods for building test token  lists

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -6,18 +6,28 @@ defmodule ParserTest do
 
   import Xbitsy.Parser
 
-  defp is_error?({:error, <<"[ERROR]"::binary, tail::binary>>}), do: true
+  # CONVENIENCE TOKEN BUILDERS
+  defp start_tokens(), do: []
+  defp finish_tokens(tokens), do: Enum.reverse(tokens)
+  defp keyword(tokens, symbol), do: [{symbol, String.upcase to_string(symbol) } | tokens]
+  defp whitespace(tokens, string), do: [{:whitespace, string} | tokens]
+
+  # CONVENIENCE VALIDATORS
+  defp is_error?({:error, <<"[ERROR]"::binary, _tail::binary>>}), do: true
   defp is_error?(_response), do: false
 
   test "throw error parsing a single keyword" do
-      tokens = [{:begin, "BEGIN"}]
+      tokens = start_tokens |> keyword(:begin) |> finish_tokens
+
       result = parse(tokens)
-      
       assert is_error?(result)
   end
 
   test "parse the bitsy null program" do
-      tokens = [{:begin, "BEGIN"}, {:whitespace, "\n"}, {:end, "END"}]
+      tokens = start_tokens 
+                    |> keyword(:begin) |> whitespace("\n") |> keyword(:end) |> 
+               finish_tokens
+
       {status, _} = parse(tokens)
       assert status == :ok
   end
