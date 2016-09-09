8d362f21668f65b1496e61c060a990af3a11acbb
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-09-08 22:53:38 -0400

    Add even simpler/cleaner helper methods for generate token list test cases

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -9,15 +9,20 @@ defmodule ParserTest do
   # CONVENIENCE TOKEN BUILDERS
   defp start_tokens(), do: []
   defp finish_tokens(tokens), do: Enum.reverse(tokens)
+  
   defp keyword(tokens, symbol), do: [{symbol, String.upcase to_string(symbol) } | tokens]
+  defp kBEGIN(tokens), do: tokens |> keyword(:begin)
+  defp kEND(tokens), do: tokens |> keyword(:end)
+
   defp whitespace(tokens, string), do: [{:whitespace, string} | tokens]
+  defp newline(tokens), do: tokens |> whitespace("\n")
 
   # CONVENIENCE VALIDATORS
   defp is_error?({:error, <<"[ERROR]"::binary, _tail::binary>>}), do: true
   defp is_error?(_response), do: false
 
   test "throw error parsing a single keyword" do
-      tokens = start_tokens |> keyword(:begin) |> finish_tokens
+      tokens = start_tokens |> kBEGIN |> finish_tokens
 
       result = parse(tokens)
       assert is_error?(result)
@@ -25,8 +30,10 @@ defmodule ParserTest do
 
   test "parse the bitsy null program" do
       tokens = start_tokens 
-                    |> keyword(:begin) |> whitespace("\n") |> keyword(:end) |> 
-               finish_tokens
+                    |> kBEGIN 
+                    |> newline 
+                    |> kEND 
+               |> finish_tokens
 
       {status, _} = parse(tokens)
       assert status == :ok
