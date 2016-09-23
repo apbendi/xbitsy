d2da34fb490cd03eb18f6b8fa1f994bf10c42fec
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-09-23 16:22:08 -0400

    Properly run the Bitsy null program

--- lib/xbitsy/runner.ex
+++ lib/xbitsy/runner.ex
@@ -1,6 +1,10 @@
 defmodule Xbitsy.Runner do
     
-    def run(_tree) do
-        {:ok, []}    
+    def run(_tree = %{kind: :program, block: %{kind: :block, statements: statement_list}}) do
+        printed_output = run_statements(statement_list)
+        {:ok, printed_output}
     end
+
+    defp run_statements(statement_list, printed_acc \\ [])
+    defp run_statements([], printed_acc), do: printed_acc
 end
